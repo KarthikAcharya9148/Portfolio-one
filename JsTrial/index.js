cbe8ba369ca48d942792932721abfe789289135e
Karthik acharya <karthikacharya9148@gmail.com>
2024-06-26 11:40:22 +0530

    commit one

---
+++ JsTrial/index.js
@@ -0,0 +1,7 @@
+// console.log('hello');
+//window.alert('This is an alert');
+
+let firstname="Rajendra";
+let email="Karthikacharya@gmail.com";
+console.log(typeof firstname);
+console.log(`your email is ${email}`);
